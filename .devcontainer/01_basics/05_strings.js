f394d041a0b8adac99c7968c73052a205ebab68c
Yukta2105 <yukta83406@gmail.com>
2026-04-05 12:13:03 +0000

    uncompleted

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,64 @@
+const name = "Yukta"
+const repoCount = 50
+
+console.log(name + repoCount + " Value") //not a good method to write loc in this case as this syntax is not good 
+//use this instead:
+
+console.log(`hello i am ${name} and my repo count is ${repoCount}`); //mostly used by webdeveloper '``' modern version
+// in "${}" symbol we can literally take any data type because it evaluates expressions and converts result into a string.
+// console.log(`Number: ${10}`);
+// console.log(`Boolean: ${true}`);
+// console.log(`Null: ${null}`);
+// console.log(`Undefined: ${undefined}`);
+
+const gameName = new String('YuktaM')
+console.log(gameName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
